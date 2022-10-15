0fcfce9553f81e14cda0444ce68abd195be5254a
Ayushi Gupta <ayushigupta8@gmail.com>
2022-10-14 17:08:49 -0700

    update: tests

--- MyLibrary/Tests/MyLibraryTests/MyLibraryIntegrationTest.swift
+++ MyLibrary/Tests/MyLibraryTests/MyLibraryIntegrationTest.swift
@@ -3,12 +3,28 @@ import XCTest
 
 
 final class MyLibraryIntegrationTest: XCTestCase {
-    func testIsWeatherApi() async throws {
+    // Integration test to check if API responds
+    func testIsWeatherApiResponse() async throws {
         let weatherService = WeatherServiceImpl()
         do {
             let temperature = try await weatherService.getTemperature()
             XCTAssertNotNil(temperature)
+        } catch {
+            print(error)
+        }
+    }
+    
+    // Checks if captured weather temperature is lucky
+    func testIsWeatherLucky() async throws {
+        let myLibrary = MyLibrary()
+        do {
+            // Check successful api request and getTemperature() execution to return Int
+            let temperature = try await myLibrary.weatherService.getTemperature()
             XCTAssert(type(of: temperature) == Int.self)
+            
+            // Check if response temp is lucky
+            let isLucky = await myLibrary.isLucky(temperature)
+            XCTAssertNotNil(isLucky)
         } catch {
             print(error)
         }
--- MyLibrary/Tests/MyLibraryTests/MyLibraryTests.swift
+++ MyLibrary/Tests/MyLibraryTests/MyLibraryTests.swift
@@ -68,18 +68,119 @@ final class MyLibraryTests: XCTestCase {
         // Then
         XCTAssertNil(isLuckyNumber)
     }
-
-    func testIsTemperatureInt() async throws {
-        let myLibrary = MyLibrary()
+    
+    // Test to check if API response JSON is of Weather type
+    func testIsResponseJSONDecodable() throws {
+        // static actual JSON Response
+        let mockResponse = """
+        {
+            "coord": {
+              "lon": -123.262,
+              "lat": 44.5646
+            },
+            "weather": [
+              {
+                "id": 804,
+                "main": "Clouds",
+                "description": "overcast clouds",
+                "icon": "04n"
+              }
+            ],
+            "base": "stations",
+            "main": {
+              "temp": 289.96,
+              "feels_like": 289.79,
+              "temp_min": 288.55,
+              "temp_max": 291.96,
+              "pressure": 1016,
+              "humidity": 80
+            },
+            "visibility": 10000,
+            "wind": {
+              "speed": 1.54,
+              "deg": 230
+            },
+            "clouds": {
+              "all": 100
+            },
+            "dt": 1664418453,
+            "sys": {
+              "type": 2,
+              "id": 2040223,
+              "country": "US",
+              "sunrise": 1664374040,
+              "sunset": 1664416792
+            },
+            "timezone": -25200,
+            "id": 5720727,
+            "name": "Corvallis",
+            "cod": 200
+          }
+        """.data(using: .utf8)!
         
+        let decoder = JSONDecoder()
         do {
-            // Get response from actual weather service API call.
-            // getTemperature() converts and returns temperature as an Integer
-            let temperature = try await myLibrary.weatherService.getTemperature()
-            XCTAssertNotNil(temperature)
-            XCTAssert(type(of: temperature) == Int.self)
+            let data = try decoder.decode(Weather.self, from: mockResponse)
+            XCTAssert(type(of: data) == Weather.self)
         } catch {
-            print(error)
+            print("error: ", error)
+        }
+    }
+    
+    // Test to check of failure when JSON is not of Weather type
+    func testIsResponseNotJSONDecodable() throws {
+        let mockResponse = """
+        {
+            "coord": {
+              "lon": -123.262,
+              "lat": 44.5646
+            },
+            "weather": [
+              {
+                "id": 804,
+                "main": "Clouds",
+                "description": "overcast clouds",
+                "icon": "04n"
+              }
+            ],
+            "base": "stations",
+            "maine": {
+              "temp": 289.96,
+              "feels_like": 289.79,
+              "temp_min": 288.55,
+              "temp_max": 291.96,
+              "pressure": 1016,
+              "humidity": 80
+            },
+            "visibility": 10000,
+            "wind": {
+              "speed": 1.54,
+              "deg": 230
+            },
+            "clouds": {
+              "all": 100
+            },
+            "dt": 1664418453,
+            "sys": {
+              "type": 2,
+              "id": 2040223,
+              "country": "US",
+              "sunrise": 1664374040,
+              "sunset": 1664416792
+            },
+            "timezone": -25200,
+            "id": 5720727,
+            "name": "Corvallis",
+            "cod": 200
+          }
+        """.data(using: .utf8)!
+        
+        let decoder = JSONDecoder()
+        do {
+            let data = try decoder.decode(Weather.self, from: mockResponse)
+            XCTAssertFalse(type(of: data) == Weather.self)
+        } catch {
+            print("JSON does not match Weather datatype. Error: ", error)
         }
     }
 
