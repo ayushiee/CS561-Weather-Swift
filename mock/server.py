fce525c65c5d9114491a9e158b6f622929f827c6
Ayushi Gupta <ayushigupta8@gmail.com>
2022-10-21 21:39:36 -0700

    fix: file type

--- mock/server.py
+++ mock/server.py
@@ -1,16 +1,17 @@
 from flask import Flask, jsonify
-from weather_response import response
+import json
 
 app = Flask(__name__);
 
 @app.route('/data/2.5/weather', methods=['GET'])
 @app.route('/data/2.5/weather?lat=44.5646&lon=-123.262&&appid=accessKey', methods=['GET'])
-def get_weather(): 
-    return jsonify(response)
+def get_weather():
+    f = open("weather_response.json", "r")
+    return json.load(f)
 
 @app.route('/', methods=['GET'])
 def default():
-    return jsonify({'response': 'Hey there' })
+    return jsonify({'response': 'Hey there'})
 
 if __name__ == '__main__':
     app.run()
